e829a921e55117e203014f01e33a46400ec4917d
jobiols <jorge.obiols@gmail.com>
2019-04-02 12:15:34 -0300

    [FIX] - hacerle un fix a point_of_sale

--- make.sh
+++ make.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
-cd data/11.0
-echo "============================================= make 11.0"
+cd odoo/11.0
 ./make.sh
-cd ../../data/11.0.debug
-echo "============================================= make 11.0.debug"
+cd ../../jeo/11.0
+./make.sh
+cd ../../jeo/11.0.debug
 ./make.sh
