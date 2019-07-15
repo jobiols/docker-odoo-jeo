4b431c810eaddc39d3beed181125012274cfeb99
jobiols <jorge.obiols@gmail.com>
2019-07-14 21:49:38 -0300

    [FIX] mejoras en extract files

--- jeo/12.0.debug/extract_dist-local-packages.sh
+++ jeo/12.0.debug/extract_dist-local-packages.sh
@@ -2,4 +2,10 @@
 ################################################################
 # Extrae los odoo packages de la imagen al host para desarrollo
 
+# remover todos los archivos incluso los hidden
+if cd /mnt/dist-local-packages/
+then
+rm -rf ..?* .[!.]* *
+fi
+# copiar los archivos de la imagen
 cp -r /usr/local/lib/python3.5/dist-packages/* /mnt/dist-local-packages/
