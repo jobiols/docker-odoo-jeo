ec4e4cc599244a103812ec43e3fc69ee81b362f2
jobiols <jorge.obiols@gmail.com>
2019-08-10 02:22:07 -0300

    [FIX] - Dockerfile v12, eliminando todo agregado salvo el entrypoint.sh - entrypoint, limpieza dejando las funciones para chequear si la bd esta lista y el agregado de unaccent - actualizacion ODOO_RELEASE=20190809 - downgrade postgres a la v10 - refactorizado de make.sh

--- make.sh
+++ make.sh
@@ -2,24 +2,40 @@
 cd odoo/11.0
 if ! ./make.sh;
 then
-    echo "Failed odoo-11.0"
+    echo "Failed odoo:11.0"
 	exit 1
 else
     echo "----------> Success odoo:11.0"
 fi
 
-cd ../../jeo/11.0
-
+cd ../12.0
 if ! ./make.sh;
 then
-    echo "Failed jeo-11.0"
+    echo "Failed odoo:12.0"
+	exit 1
+else
+    echo "----------> Success odoo:12.0"
+fi
+
+cd ../../jeo/11.0
+if ! ./make.sh;
+then
+    echo "Failed odoo-jeo:11.0"
 	exit 1
 else
     echo "----------> Success odoo-jeo:11.0"
 fi
 
-cd ../../jeo/11.0.debug
+cd ../12.0
+if ! ./make.sh;
+then
+    echo "Failed odoo-jeo:12.0"
+	exit 1
+else
+    echo "----------> Success odoo-jeo:12.0"
+fi
 
+cd ../11.0.debug
 if ! ./make.sh;
 then
     echo "Failed jeo-11.0.debug"
@@ -28,28 +44,7 @@ else
     echo "----------> Success odoo-jeo:11.0.debug"
 fi
 
-cd ../../odoo/12.0
-
-if ! ./make.sh;
-then
-    echo "Failed odoo-12.0"
-	exit 1
-else
-    echo "----------> Success odoo:12.0"
-fi
-
-cd ../../jeo/12.0
-
-if ! ./make.sh;
-then
-    echo "Failed jeo-12.0"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:12.0"
-fi
-
-cd ../../jeo/12.0.debug
-
+cd ../12.0.debug
 if ! ./make.sh;
 then
     echo "Failed jeo-12.0.debug"
