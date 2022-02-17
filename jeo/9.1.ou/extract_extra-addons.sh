e493dc94028da8a5028c15577b77ddd44b833e1a
Jorge Obiols <jorge.obiols@gmail.com>
2022-02-17 12:29:35 -0300

    [REM] 9.1.ou

--- jeo/9.1.ou/extract_extra-addons.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-################################################################
-# Extrae los odoo extra addons de la imagen al host para desarrollo
-
-rm -r /mnt/extra-addons/*
-cp -r /opt/odoo/extra-addons/* /mnt/extra-addons/
-
--- jeo/9.1.ou/make.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# creado para openupgrade
-sd build --rm=true -t jobiols/odoo-jeo:9.1.ou.debug ./
-sd build --rm=true -t jobiols/odoo-jeo:9.1.ou ./
