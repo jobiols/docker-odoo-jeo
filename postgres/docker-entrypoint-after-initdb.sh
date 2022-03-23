28cca52290e0b5c591fa729f69d513cb0719fe1d
Jorge Obiols <jorge.obiols@gmail.com>
2022-03-23 11:51:36 -0300

    [ADD]  Postgres

---
+++ postgres/docker-entrypoint-after-initdb.sh
@@ -0,0 +1,17 @@
+
+
+#!/usr/bin/env bash
+
+mkdir $PGDATA/conf.d
+sed -i "s/#include_dir = 'conf.d'/include_dir = 'conf.d'/" "$PGDATA/postgresql.conf"
+cp /docker-entrypoint-initdb.d/*.conf $PGDATA/conf.d/
+
+echo "Stop PG"
+pg_ctl -D "$PGDATA" -m fast -w stop
+
+echo "Start PG"
+pg_ctl -D "$PGDATA" \
+    -o "-c listen_addresses='localhost'" \
+    -w start
+
+echo "Reload done"
---
+++ postgres/make.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sd build --rm=true -t postgres:14.2-cge ./
