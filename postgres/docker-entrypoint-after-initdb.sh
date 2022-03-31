76e46a257aa775a885d69a0de43d7fd7a7ed456b
Jorge Obiols <jorge.obiols@gmail.com>
2022-03-31 17:25:09 -0300

    [FIX] postgres

--- postgres/docker-entrypoint-after-initdb.sh
+++ postgres/docker-entrypoint-after-initdb.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ${PGDATA}/conf.d && \
+sed -i '/^# "local".*/a local\todoo\t\todoo\t\t\t\t\tmd5' "${PGDATA}/pg_hba.conf" && \
 sed -i "s/#include_dir = '...'/include_dir = 'conf.d'/" "${PGDATA}/postgresql.conf" && \
 cp -f /docker-entrypoint-initdb.d/*.conf ${PGDATA}/conf.d && \
 chown -R postgres:postgres ${PGDATA}/conf.d && \
