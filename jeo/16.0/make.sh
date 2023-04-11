f772dd4d08f6fafdc192673d0d41b467986ac28d
Jorge Obiols <jorge.obiols@gmail.com>
2023-04-11 11:57:01 -0300

    [FIX] actualizacion librerias

--- jeo/16.0/make.sh
+++ jeo/16.0/make.sh
@@ -2,7 +2,11 @@
 sd build --rm=true -t jobiols/odoo-jeo:16.0 ./
 result=$?
 if [ "$result" -eq 0 ]; then
+
     sd push jobiols/odoo-jeo:16.0
+
 else
+
     echo "Falló la creación de la imagen"
+    
 fi
