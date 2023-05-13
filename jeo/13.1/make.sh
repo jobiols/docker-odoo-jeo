a99dd96a4bfd8f5a301fc0d0a90d2607520e182d
Jorge Obiols <jorge.obiols@gmail.com>
2023-05-13 16:51:14 -0300

    correccion imagen multisitio

--- jeo/13.1/make.sh
+++ jeo/13.1/make.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 sd build --rm=true -t jobiols/odoo-jeo:13.1 ./
-sd push jobiols/odoo-jeo:13.1
+result=$?
+if [ "$result" -eq 0 ]; then
+
+    sd push jobiols/odoo-jeo:13.1
+else
+    echo "Falló la creación de la imagen"
+fi
+exit $return_code
