c68d3f01578cfe572a921507f611c574b928718c
Jorge Obiols <jorge.obiols@gmail.com>
2023-11-24 20:11:28 -0300

    [ADD] 18.0

---
+++ jeo/18.0.debug/make.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sd build --rm=true -t jobiols/odoo-jeo:17.0.debug ./
+result=$?
+if [ "$result" -eq 0 ]; then
+    sd push jobiols/odoo-jeo:17.0.debug
+else
+    echo "Falló la creación de la imagen"
+fi
