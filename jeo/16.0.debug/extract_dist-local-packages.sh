3cf72ba111e2c9bf9061d61a1139d58dc3837bd4
Jorge Obiols <jorge.obiols@gmail.com>
2023-12-02 11:42:40 -0300

    [FIX] quitar rm de dist-packages

--- jeo/16.0.debug/extract_dist-local-packages.sh
+++ jeo/16.0.debug/extract_dist-local-packages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ################################################################
 # Extrae los odoo packages de la imagen al host para desarrollo
-# revision 2021-10-08
+# revision 2023-11-02
 
-rm -r /mnt/dist-local-packages/*
 cp -r /usr/local/lib/python3.9/dist-packages/* /mnt/dist-local-packages/
