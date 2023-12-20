5f78bcefa15fae909f9c664368b857502a8dff23
Jorge Obiols <jorge.obiols@gmail.com>
2023-12-20 09:32:07 -0300

    [FIX] bump version paramiko

--- jeo/16.0.debug/make.sh
+++ jeo/16.0.debug/make.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 sd build --rm=true -t jobiols/odoo-jeo:16.0.debug ./
-sd push jobiols/odoo-jeo:16.0.debug
+# result=$?
+# if [ "$result" -eq 0 ]; then
+#     sd push jobiols/odoo-jeo:16.0.debug
+# else
+#     echo "Falló la creación de la imagen"
+# fi
