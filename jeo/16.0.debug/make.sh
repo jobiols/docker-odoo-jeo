a91096f29c2da05a5cf4511b3c8ecfe09c567dbc
Jorge Obiols <jorge.obiols@gmail.com>
2024-01-12 15:36:26 -0300

    [FIX] subida de imagen 16.0.debug

--- jeo/16.0.debug/make.sh
+++ jeo/16.0.debug/make.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 sd build --rm=true -t jobiols/odoo-jeo:16.0.debug ./
-# result=$?
-# if [ "$result" -eq 0 ]; then
-#     sd push jobiols/odoo-jeo:16.0.debug
-# else
-#     echo "Falló la creación de la imagen"
-# fi
+result=$?
+if [ "$result" -eq 0 ]; then
+    sd push jobiols/odoo-jeo:16.0.debug
+else
+    echo "Falló la creación de la imagen"
+fi
