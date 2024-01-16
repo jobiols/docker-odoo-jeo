a13437fd44c575e9827cc3b33093082c486cbfcd
Jorge Obiols <jorge.obiols@gmail.com>
2024-01-16 13:51:29 -0300

    [FIX] corregir imagen 13.0 agregar 13.0.hack

--- jeo/13.0/make.sh
+++ jeo/13.0/make.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 sd build --rm=true -t jobiols/odoo-jeo:13.0 ./
-sd push jobiols/odoo-jeo:13.0
+result=$?
+if [ "$result" -eq 0 ]; then
+
+    sd push jobiols/odoo-jeo:13.0
+else
+    echo "Falló la creación de la imagen"
+fi
+exit $return_code
