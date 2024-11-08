2a177fbe406c486021aadd2f11aa5098c40c7583
Jorge Obiols <jorge.obiols@gmail.com>
2024-11-08 08:51:42 -0300

    [FIX] dbtools 1.4.2

--- dbtools/make.sh
+++ dbtools/make.sh
@@ -3,7 +3,7 @@ sd build --rm=true -t jobiols/dbtools:1.4.1 ./
 
 result=$?
 if [ "$result" -eq 0 ]; then
-    sd push jobiols/dbtools:1.4.1
+    sd push jobiols/dbtools:1.4.2
 else
     echo "Falló la creación de la imagen"
 fi
