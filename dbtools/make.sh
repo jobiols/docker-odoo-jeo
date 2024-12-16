82a08ded7abcd13953d55f9aabf57fef598248df
Jorge Obiols <jorge.obiols@gmail.com>
2024-12-16 13:57:00 -0300

    [FIX] Dbtools 1.4.6 apline

--- dbtools/make.sh
+++ dbtools/make.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sd build --rm=true -t jobiols/dbtools:1.4.4 ./
+sd build --rm=true -t jobiols/dbtools:1.4.6 ./
 
 result=$?
 if [ "$result" -eq 0 ]; then
-    sd push jobiols/dbtools:1.4.4
+    sd push jobiols/dbtools:1.4.6
 else
     echo "Falló la creación de la imagen"
 fi
