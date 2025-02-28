fde5f40d34ad1c6082e3183790c396ed6c82bd43
Jorge Obiols <jorge.obiols@gmail.com>
2025-02-28 19:25:12 -0300

    [WIP] parchado v17 y desarrollando v17.2

---
+++ jeo/17.2/make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sd build --rm=true -t jobiols/odoo-jeo:17.0 ./ 
+# result=$?
+# if [ "$result" -eq 0 ]; then
+#     sd push jobiols/odoo-jeo:17.0
+# else
+#     echo "Falló la creación de la imagen"
+# fi
+# exit $result
