de114173687d9e326b9ef3186c9a5bfb7f423825
Jorge Obiols <jorge.obiols@gmail.com>
2025-05-06 10:55:55 -0300

    [FIX] ajustes en v18

--- jeo/18.0.debug/extract_dist-local-packages.sh
+++ jeo/18.0.debug/extract_dist-local-packages.sh
@@ -4,4 +4,4 @@
 # revision 2023-11-17
 
 rm -r /mnt/dist-local-packages/*
-cp -r /usr/local/lib/python3.10/dist-packages/* /mnt/dist-local-packages/
+cp -r /usr/local/lib/python3.12/dist-packages/* /mnt/dist-local-packages/
