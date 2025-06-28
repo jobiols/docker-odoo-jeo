e335eaf0edbc8b4872b49c15dafa1216715c8379
Jorge Obiols <jorge.obiols@gmail.com>
2025-06-28 17:05:30 -0300

    [WIP] trabajando en el dockerfile

--- jeo/16.0/make.sh
+++ jeo/16.0/make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sd build --rm=true -t jobiols/odoo-jeo:16.01 ./
+#sd build --rm=true --no-cache -t jobiols/odoo-jeo:16.01 ./
+sudo docker build --build-arg ODOO_RELEASE=$(date +%Y%m%d) --tag jobiols/odoo-jeo:16.01 ./
 result=$?
 if [ "$result" -eq 0 ]; then
     sd push jobiols/odoo-jeo:16.01
