1b1c3dbb6bdc14ebc6465634a925d1af4b42fa9a
Jorge Obiols <jorge.obiols@gmail.com>
2025-06-30 09:15:14 -0300

    remove sd in makefile

--- jeo/16.0.debug/make.sh
+++ jeo/16.0.debug/make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sd build --rm=true -t jobiols/odoo-jeo:16.0.debug ./
+sudo docker build -t jobiols/odoo-jeo:16.0.debug ./
 result=$?
 if [ "$result" -eq 0 ]; then
     sd push jobiols/odoo-jeo:16.0.debug
