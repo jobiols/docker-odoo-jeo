ca68098acd5aea62d71708d78a80e0fcd2f313fb
Jorge Obiols <jorge.obiols@gmail.com>
2025-06-30 09:12:59 -0300

    aplicar a la version correcta

--- jeo/16.0/make.sh
+++ jeo/16.0/make.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-#sd build --rm=true --no-cache -t jobiols/odoo-jeo:16.01 ./
-sudo docker build --build-arg ODOO_RELEASE=$(date +%Y%m%d) --tag jobiols/odoo-jeo:16.01 ./
+sudo docker build --build-arg ODOO_RELEASE=$(date +%Y%m%d) --tag jobiols/odoo-jeo:16.0 ./
 result=$?
 if [ "$result" -eq 0 ]; then
-    sd push jobiols/odoo-jeo:16.01
+    sd push jobiols/odoo-jeo:16.0
 else
     echo "Falló la creación de la imagen"
 fi
