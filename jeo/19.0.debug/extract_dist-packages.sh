d2f280fde40395aeb8d3aa36ca75566e02608fca
Jorge Obiols <jorge.obiols@gmail.com>
2025-08-03 12:13:06 -0300

    [FIX] 20250801 + Version 19

--- jeo/19.0.debug/extract_dist-packages.sh
+++ jeo/19.0.debug/extract_dist-packages.sh
@@ -3,5 +3,5 @@
 # Extrae los odoo packages de la imagen al host para desarrollo
 # revision 2023-11-17
 
-rm -rf /mnt/dist-local-packages/*
-cp -r /usr/local/lib/python3.12/dist-packages/* /mnt/dist-local-packages/
+rm -r /mnt/dist-packages/*
+cp -r /usr/lib/python3/dist-packages/* /mnt/dist-packages/
--- jeo/19.0.debug/make.sh
+++ jeo/19.0.debug/make.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sd build --rm=true -t jobiols/odoo-ocb:16.0 ./
+sd build --rm=true -t jobiols/odoo-jeo:19.0.debug ./
 result=$?
 if [ "$result" -eq 0 ]; then
-    sd push jobiols/odoo-ocb:16.0
+    sd push jobiols/odoo-jeo:19.0.debug
 else
     echo "Falló la creación de la imagen"
 fi
-exit $return_code
+exit $result
