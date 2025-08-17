ba04fa4e00e68eaf8330f17722b3ecd1752e9931
Jorge Obiols <jorge.obiols@gmail.com>
2025-08-17 16:28:58 -0300

    [FIX] debugging

--- jeo/17.0.debug/extract_dist-local-packages.sh
+++ jeo/17.0.debug/extract_dist-local-packages.sh
@@ -3,5 +3,4 @@
 # Extrae los odoo packages de la imagen al host para desarrollo
 # revision 2023-11-17
 
-rm -r /mnt/dist-local-packages/*
 cp -r /usr/local/lib/python3.10/dist-packages/* /mnt/dist-local-packages/
