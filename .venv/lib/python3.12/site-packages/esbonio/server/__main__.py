a985005442cb920f65ea3eaeb1fcdd795b7ab862
Jorge Obiols <jorge.obiols@gmail.com>
2026-06-02 19:01:33 -0300

    [REM] .venv

--- .venv/lib/python3.12/site-packages/esbonio/server/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-from esbonio.server.cli import main
-
-sys.exit(main())
--- .venv/lib/python3.12/site-packages/esbonio/sphinx_agent/__main__.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-try:
-    from esbonio.sphinx_agent.server import main
-except ImportError:
-    from .server import main
-
-asyncio.run(main())
